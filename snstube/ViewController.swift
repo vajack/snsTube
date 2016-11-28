5729c72a5319f6b2df969d7dcbd219b52ce13b09
Yuki-Ikegami <fp25657.hawks@icloud.com>
2016-11-28 17:22:30 +0900

    first commit

---
+++ snstube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  snstube
+//
+//  Created by IkegamiYuki on 2016/11/28.
+//  Copyright © 2016 IkegamiYuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
